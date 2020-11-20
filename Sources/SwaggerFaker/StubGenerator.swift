e0e2e2221d5abc30bb40ded4aa43c4b7914e0cbe
Sébastien Duperron <sebastien@code-craftsman.fr>
2020-11-20 22:10:47 +0100

    Extract GeneratorGroup into its own file

--- Sources/SwaggerFaker/StubGenerator.swift
+++ Sources/SwaggerFaker/StubGenerator.swift
@@ -1,25 +1,6 @@
 import Foundation
 import Swagger
 
-struct GeneratorGroup {
-    let booleanGenerator: BooleanGenerator
-    let integerGenerator: IntegerGenerator
-    let numberGenerator: NumberGenerator
-    let stringGenerator: StringGenerator
-    let arrayGenerator: ArrayGenerator
-}
-
-extension GeneratorGroup {
-    init(configuration: GeneratorConfigurations = .default) {
-        let dateGenerator = DateGenerator(config: configuration.date)
-        self.init(booleanGenerator: .init(config: configuration.boolean),
-                  integerGenerator: .init(config: configuration.integer),
-                  numberGenerator: .init(config: configuration.number),
-                  stringGenerator: .init(config: configuration.string, dateGenerator: dateGenerator),
-                  arrayGenerator: .init(config: configuration.array))
-    }
-}
-
 public struct StubGenerator {
     private let generators: GeneratorGroup
 
